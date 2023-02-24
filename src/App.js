f1af8bc05fb0c85b605423013fc625691cef4342
AbdulRehma-n <abdulrehman6214311@gmail.com>
2023-02-24 21:23:54 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
 import Navbar from "./component/navbar/Navbar";
-
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Footer from "./component/Footer/Footer";
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-      <Navbar />
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Navbar.css";
+import pic from "../../Assets/profile.jpeg";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  const [active, isActive] = React.useState(false);
+  const [active, setActive] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const isActive = () => {
+    window.scrollY ? setActive(true) : setActive(false);
+  };
+
+  const currentUser = {
+    id: 1,
+    userName: "ali",
+    isSeller: true,
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
   return (
     <>
       <div className={active ? "navbar active" : "navbar"}>
         <div className="container">
           <div className="logo">
-            <span>Fiverr</span>
-            <span className="dot">.</span>
+            <Link to="/" className="link">
+              <span>Fiverr</span>
+
+              <span className="dot">.</span>
+            </Link>
           </div>
           <div className="link">
             <span className="text">Fiver Business</span>
             <span className="text">Fiver Explore</span>
             <span className="text">English</span>
             <span className="text">Sign in</span>
-            <span className="text">Become a Seller</span>
-            <button>Join </button>
+            {!currentUser?.isSeller && (
+              <span className="text">Become a Seller</span>
+            )}
+            {!currentUser && <button>Join </button>}
+            {currentUser && (
+              <div className="user" onClick={() => setOpen(!open)}>
+                <img src={pic} className="img" />
+                <span>{currentUser.userName}</span>
+                {open && (
+                  <div className="option">
+                    {currentUser?.isSeller && (
+                      <>
+                        <span>Gig</span>
+                        <span>Add new gig</span>
+                      </>
+                    )}
+                    <span>Order</span>
+                    <span>Message</span>
+                    <span>logout</span>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
         {active && (
